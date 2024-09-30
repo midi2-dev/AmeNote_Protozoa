bb65e8cc6a597af1a5bd7dc58b143013d2cb458e
Andrew Mee <andrew@midi2.dev>
2024-09-30 11:24:47 +1000

    Fix FB indexes - fixes #5 - part 2

--- UUT/USB_FunctionBlocks/main.cpp
+++ UUT/USB_FunctionBlocks/main.cpp
@@ -197,17 +197,17 @@ void functionblock(uint8_t fbIdx, uint8_t filter){
     if(filter & 0x2) {
         if(fbIdx==0 || fbIdx==0xFF) {
             std::array<uint32_t, 4> UMP = UMPMessage::mtFFunctionBlockNameNotify(
-                    1, 0, (uint8_t *) "FB 1", 4);
+                    0, 0, (uint8_t *) "FB 1", 4);
             tud_ump_write(0,UMP.data(),4);
         }
         if(fbIdx==1 || fbIdx==0xFF) {
             std::array<uint32_t, 4> UMP = UMPMessage::mtFFunctionBlockNameNotify(
-                    2, 0, (uint8_t *) "FB 2", 4);
+                    1, 0, (uint8_t *) "FB 2", 4);
             tud_ump_write(0,UMP.data(),4);
         }
         if(fbIdx==2 || fbIdx==0xFF) {
             std::array<uint32_t, 4> UMP = UMPMessage::mtFFunctionBlockNameNotify(
-                    3, 0, (uint8_t *) "FB 3", 4);
+                    2, 0, (uint8_t *) "FB 3", 4);
             tud_ump_write(0,UMP.data(),4);
         }
     }
